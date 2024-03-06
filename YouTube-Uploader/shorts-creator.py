cd68dc436543ea39da1d1074395423d1cdc73ca3
shreesat.sahu@allendigital.in+1-at-684168337650 <69542882+CreatoPro@users.noreply.github.com>
2024-03-06 15:13:20 +0530

    new mods

--- YouTube-Uploader/shorts-creator.py
+++ YouTube-Uploader/shorts-creator.py
@@ -12,137 +12,162 @@ def process_video(url, output_modifier="modified"):
     except AgeRestrictedError:
         print(f"The video with ID '{url}' is age-restricted and cannot be accessed without logging in.")
         return
-    
-    stream = yt.streams.filter(file_extension='mp4',progressive=True).order_by('resolution').first()
-    custom_filename = yt.title + ".mp4"
-
-    # renamer
-    reversed_string = custom_filename[::-1]
-    index = reversed_string.find('|')
-    if index != -1:
-        sliced_reversed_string = reversed_string[:index]
-    else:
-        sliced_reversed_string = reversed_string
-
-    final_name = sliced_reversed_string[::-1]
-
-    save_path = ''
-    stream.download(output_path=save_path, filename=final_name)
-
-    resolution = stream.resolution if stream.resolution else "Unknown Resolution"
-
-    print(custom_filename + ' Video downloaded successfully!')
-    print('Resolution:', resolution)
-
-    output_video = f'{output_modifier}_{final_name}'
-    cap = cv2.VideoCapture(final_name)
-    if not cap.isOpened():
-        print("Error: Couldn't open video file.")
+    except Exception as e:
+        print(f"An error occurred while processing the URL '{url}': {e}")
         return
 
-    target_width, target_height = 1080, 1920
+    try:
+        stream = yt.streams.filter(file_extension='mp4',progressive=True).order_by('resolution').first()
+        custom_filename = yt.title + ".mp4"
 
-    # Get input video properties
-    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    fps = cap.get(cv2.CAP_PROP_FPS)
+        # renamer
+        reversed_string = custom_filename[::-1]
+        index = reversed_string.find('|')
+        if index != -1:
+            sliced_reversed_string = reversed_string[:index]
+        else:
+            sliced_reversed_string = reversed_string
 
-    # Calculate the aspect ratios
-    aspect_ratio_input = width / height
-    aspect_ratio_target = target_width / target_height
+        final_name = sliced_reversed_string[::-1]
 
-    # Calculate the new size
-    if aspect_ratio_input > aspect_ratio_target:
-        # Fit width
-        new_width = target_width
-        new_height = int(new_width / aspect_ratio_input)
-    else:
-        # Fit height
-        new_height = target_height
-        new_width = int(new_height * aspect_ratio_input)
+        save_path = ''
+        stream.download(output_path=save_path, filename=final_name)
 
-    # Calculate margins
-    margin_top = (target_height - new_height) // 2
-    margin_bottom = target_height - new_height - margin_top
-    margin_left = (target_width - new_width) // 2
-    margin_right = target_width - new_width - margin_left
+        resolution = stream.resolution if stream.resolution else "Unknown Resolution"
 
-    # Create black background canvas
-    black_background = np.zeros((target_height, target_width, 3), dtype=np.uint8) * 255
+        print(custom_filename + ' Video downloaded successfully!')
+        print('Resolution:', resolution)
 
-    # Create VideoWriter object
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Specify codec
-    out = cv2.VideoWriter(output_video, fourcc, fps, (target_width, target_height))
+        output_video = f'{output_modifier}_{final_name}'
+        cap = cv2.VideoCapture(final_name)
+        if not cap.isOpened():
+            print("Error: Couldn't open video file.")
+            return
 
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            break
+        target_width, target_height = 1080, 1920
 
-        # Resize the frame
-        resized_frame = cv2.resize(frame, (new_width, new_height))
+        # Get input video properties
+        width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        fps = cap.get(cv2.CAP_PROP_FPS)
 
-        # Add the resized frame to the black background canvas
-        black_background[margin_top:margin_top + new_height, margin_left:margin_left + new_width] = resized_frame
+        # Calculate the aspect ratios
+        aspect_ratio_input = width / height
+        aspect_ratio_target = target_width / target_height
 
-        # Write the black background with the resized frame to the output video
-        out.write(black_background)
+        # Calculate the new size
+        if aspect_ratio_input > aspect_ratio_target:
+            # Fit width
+            new_width = target_width
+            new_height = int(new_width / aspect_ratio_input)
+        else:
+            # Fit height
+            new_height = target_height
+            new_width = int(new_height * aspect_ratio_input)
 
-    # Release VideoCapture and VideoWriter
-    cap.release()
-    out.release()
-    cv2.destroyAllWindows()
+        # Calculate margins
+        margin_top = (target_height - new_height) // 2
+        margin_bottom = target_height - new_height - margin_top
+        margin_left = (target_width - new_width) // 2
+        margin_right = target_width - new_width - margin_left
 
-    print(f'Output file "{output_video}" created successfully.')
+        # Create black background canvas
+        black_background = np.zeros((target_height, target_width, 3), dtype=np.uint8) * 255
 
-    # Extract audio and merge it with the modified video
-    audio_clip = mp.AudioFileClip(final_name)
-    audio_clip.write_audiofile(f'{output_modifier}_audio.mp3')
+        # Create VideoWriter object
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Specify codec
+        out = cv2.VideoWriter(output_video, fourcc, fps, (target_width, target_height))
 
-    final_video_clip = mp.VideoFileClip(output_video)
-    final_video_clip.write_videofile(f'{output_modifier}_with_audio.mp4', audio=f'{output_modifier}_audio.mp3')
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
 
-    os.rename(f'{output_modifier}_with_audio.mp4', f'{final_name}_edited.mp4')
-    os.remove(f'{output_modifier}_audio.mp3')
-    os.remove(f'{final_name}')
-    os.remove("modified_" f'{final_name}')
-    print('Audio extracted and merged with the modified video successfully.')
+            # Resize the frame
+            resized_frame = cv2.resize(frame, (new_width, new_height))
 
-    ##############################
-    # Create output folder if it doesn't exist
-    namewithoutextentoin = final_name.replace(".mp4", '')
+            # Add the resized frame to the black background canvas
+            black_background[margin_top:margin_top + new_height, margin_left:margin_left + new_width] = resized_frame
 
-    video_path = f'{final_name}_edited.mp4'
-    output_folder = f'{namewithoutextentoin}_shorts'
-    os.makedirs(output_folder, exist_ok=True)
+            # Write the black background with the resized frame to the output video
+            out.write(black_background)
 
-    # Open the video file
-    video = VideoFileClip(video_path)
+        # Release VideoCapture and VideoWriter
+        cap.release()
+        out.release()
+        cv2.destroyAllWindows()
 
-    # Calculate the duration of the video in seconds
-    duration = video.duration
+        print(f'Output file "{output_video}" created successfully.')
 
-    # Calculate the number of clips and duration of each clip
-    num_clips = int(duration // 30) + 1
-    clip_duration = 30
+        # Extract audio and merge it with the modified video
+        audio_clip = mp.AudioFileClip(final_name)
+        audio_clip.write_audiofile(f'{output_modifier}_audio.mp3')
 
-    # Extract each clip
-    for i in range(num_clips):
-        # Set start and end times for the clip
-        start_time = i * clip_duration
-        end_time = min((i + 1) * clip_duration, duration)
-        outro = VideoFileClip("outro.mp4")
-        # Extract and save the clip
-        clip1 = video.subclip(start_time, end_time)
-        clip = concatenate_videoclips([clip1, outro])
-        clip_name = f"{namewithoutextentoin} Shorts {i + 1}.mp4"
-        clip_path = os.path.join(output_folder, clip_name)
-        clip.write_videofile(clip_path, codec='libx264', audio_codec="aac")
+        final_video_clip = mp.VideoFileClip(output_video)
+        final_video_clip.write_videofile(f'{output_modifier}_with_audio.mp4', audio=f'{output_modifier}_audio.mp3')
 
-    # Close the video file
-    video.close()
+        os.rename(f'{output_modifier}_with_audio.mp4', f'{final_name}_edited.mp4')
+        os.remove(f'{output_modifier}_audio.mp3')
+        os.remove(f'{final_name}')
+        os.remove("modified_" f'{final_name}')
+        print('Audio extracted and merged with the modified video successfully.')
+
+        ##############################
+        # Create output folder if it doesn't exist
+        namewithoutextentoin = final_name.replace(".mp4", '')
+
+        video_path = f'{final_name}_edited.mp4'
+        output_folder = f'{namewithoutextentoin}_shorts'
+        os.makedirs(output_folder, exist_ok=True)
+
+        # Open the video file
+        video = VideoFileClip(video_path)
+
+        # Calculate the duration of the video in seconds
+        duration = video.duration
+
+        # Calculate the number of clips and duration of each clip
+        num_clips = int(duration // 30) + 1
+        clip_duration = 30
+
+        # Extract each clip
+        for i in range(num_clips):
+            # Set start and end times for the clip
+            start_time = i * clip_duration
+            end_time = min((i + 1) * clip_duration, duration)
+            outro = VideoFileClip("outro.mp4")
+            # Extract and save the clip
+            clip1 = video.subclip(start_time, end_time)
+            clip = concatenate_videoclips([clip1, outro])
+            clip_name = f"{namewithoutextentoin} Shorts {i + 1}.mp4"
+            clip_path = os.path.join(output_folder, clip_name)
+            clip.write_videofile(clip_path, codec='libx264', audio_codec="aac")
+
+        # Close the video file
+        video.close()
+    except Exception as e:
+        print(f"An error occurred while processing the video '{url}': {e}")
+        return
 
 # Example usage:
-video_urls = ['https://www.youtube.com/watch?v=ppV59MZ1hLU']
+video_urls = [
+    "https://www.youtube.com/watch?v=Vqa7wLcQr38"
+    "https://www.youtube.com/watch?v=4e_dnzv6L00",
+    "https://www.youtube.com/watch?v=hwYnUa0gl4Y",
+    "https://www.youtube.com/watch?v=p276Zau2SUc",
+    "https://www.youtube.com/watch?v=0glPxAd9Vsg",
+    "https://www.youtube.com/watch?v=iQLimUMjEhE",
+    "https://www.youtube.com/watch?v=UEKGCwqDC_w",
+    "https://www.youtube.com/watch?v=_gDZFOZ5W34",
+    "https://www.youtube.com/watch?v=SYBoO7YiMN4",
+    "https://www.youtube.com/watch?v=F5NBcWquFVk",
+    "https://www.youtube.com/watch?v=AcC_cxQbiu8",
+    "https://www.youtube.com/watch?v=hGuVkv3g6aQ",
+    "https://www.youtube.com/watch?v=gcS854lt7PI",
+    "https://www.youtube.com/watch?v=JGKQjTT-oxg",
+    "https://www.youtube.com/watch?v=tyYy_3tLt6k",
+    "https://www.youtube.com/watch?v=XpXLLLornKc",
+    "https://www.youtube.com/watch?v=SXpCGNRMNlY"
+]
 for url in video_urls:
     process_video(url, output_modifier='modified')
--- YouTube-Uploader/shorts-creators.py
+++ YouTube-Uploader/shorts-creators.py
@@ -1,40 +1,51 @@
-import time
-from selenium import webdriver 
-from selenium.webdriver.chrome.options import Options
-import webbrowser
-import os
-import pyautogui
+# import time
+# from selenium import webdriver 
+# from selenium.webdriver.chrome.options import Options
+# import webbrowser
+# import os
+# import pyautogui
 
-chrome_path = r'C:\ Program Files (x86)\ Google\ Chrome\ Application\ chrome.exe %s'
+# chrome_path = r'C:\ Program Files (x86)\ Google\ Chrome\ Application\ chrome.exe %s'
 
 
-webbrowser.get(chrome_path)
-webbrowser.open_new_tab("https://studio.youtube.com/")
-time.sleep(10)
-create = pyautogui.locateCenterOnScreen('create.png')#If the file is not a png file it will not work
-print(create)
-pyautogui.moveTo(create)
-time.sleep(2)
-pyautogui.click()
-time.sleep(2)
-upload = pyautogui.locateCenterOnScreen('upload.png')
-print(upload)
-pyautogui.moveTo(upload)
-time.sleep(2)
-pyautogui.click()
-time.sleep(2)
-select = pyautogui.locateCenterOnScreen('select.png')
-print(select)
-pyautogui.moveTo(select)
-time.sleep(2)
-pyautogui.click()
+# webbrowser.get(chrome_path)
+# webbrowser.open_new_tab("https://studio.youtube.com/")
+# time.sleep(10)
+# create = pyautogui.locateCenterOnScreen('create.png')#If the file is not a png file it will not work
+# print(create)
+# pyautogui.moveTo(create)
+# time.sleep(2)
+# pyautogui.click()
+# time.sleep(2)
+# upload = pyautogui.locateCenterOnScreen('upload.png')
+# print(upload)
+# pyautogui.moveTo(upload)
+# time.sleep(2)
+# pyautogui.click()
+# time.sleep(2)
+# select = pyautogui.locateCenterOnScreen('select.png')
+# print(select)
+# pyautogui.moveTo(select)
+# time.sleep(2)
+# pyautogui.click()
 
-current_folder = os.getcwd()
-print("Current folder:", current_folder)
+# current_folder = os.getcwd()
+# print("Current folder:", current_folder)
 
-time.sleep(2)
-file_name = pyautogui.locateCenterOnScreen('file_name.png')
-print(file_name)
-pyautogui.moveTo(file_name)
-time.sleep(2)
-pyautogui.click()
+# time.sleep(2)
+# file_name = pyautogui.locateCenterOnScreen('file_name.png')
+# print(file_name)
+# pyautogui.moveTo(file_name)
+# time.sleep(2)
+# pyautogui.click()
+
+
+
+from pytube import YouTube
+
+# Replace 'VIDEO_URL' with the URL of the age-restricted video
+video_url = 'https://www.youtube.com/watch?v=Vqa7wLcQr38'
+
+
+yt = YouTube(video_url)
+yt.streams.first().download()
